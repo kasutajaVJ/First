06d8d96f78165a64331f2ce6f19b7815468585c0
opilane <vanessa.jalakas@gmail.com>
2018-09-05 16:01:35 +0300

    Add message with Toaster

--- app/src/main/java/com/example/vanessajalakas/first/MainActivity.java
+++ app/src/main/java/com/example/vanessajalakas/first/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button nupp;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         nupp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "http://tallinnzoo.ee";
+                String url = "http://tallinnzoo.ee/en";
                 Intent zoo = new Intent(Intent.ACTION_VIEW);
                 zoo.setData(Uri.parse(url));
                 startActivity(zoo);
@@ -29,7 +32,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void vajutus(View view) {
+        // Using Intent to change my screen views
         Intent pildid = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(pildid);
+        message("Vahetasid vaadet");
+        Log.i(TAG, "Changed view");
+    }
+
+    //Toast
+    public void message(String message) {
+        //standard text structure
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
